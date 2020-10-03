a2d75c00be482910c1baf28f69ba3c2826e7ea9e
Gleb <pglb@yandex.ru>
2020-10-03 23:21:39 +0400

    init: typical array problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
+  if (array == undefined) {
+    return 0;
+  }  else if (array.length == 0) {
   return 0;
+} else {  
+  let min = array[0];
+  for (let i = 0; i < array.length; i++) {
+      if (min > array[i]) min = array[i];
+  }
+  return min;
+}  
 }
 
 exports.max = function max (array) {
+  if (array == undefined) {
+    return 0;
+  }  else if (array.length == 0) {
   return 0;
+} else {  
+  let max = array[0];
+  for (let i = 0; i < array.length; i++) {
+      if (max < array[i]) max = array[i];
+  }
+  return max;
+}  
 }
 
 exports.avg = function avg (array) {
+  if (array == undefined) {
+    return 0;
+  }  else if (array.length == 0) {
   return 0;
+} else {  
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+   }
+  let avg = sum / array.length;
+  return avg;
+} 
 }
